9a441acd12bbd6a4e2cdd7912fc3e4ac127dbbb4
Ubuntu <ubuntu@ip-172-31-2-159.us-west-1.compute.internal>
2019-12-11 05:07:41 +0000

    change dates to deal with schuler timedate issues

--- pset-final/dags/01_kneaddata.py
+++ pset-final/dags/01_kneaddata.py
@@ -5,12 +5,12 @@ DAG to run kneaddata(data cleaner) locally for testing purposes
 
 from airflow.operators.bash_operator import BashOperator
 from airflow.models import DAG
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # Basic arguemnts to pass to Airflow
 args = {
     'owner': 'airflow',
-    'start_date': datetime.now(),
+    'start_date': datetime.now() - timedelta(days = 1),
 }
 
 # Create the head dag
@@ -21,5 +21,5 @@ dag = DAG(
 
 kneaddata = BashOperator(
     task_id='kneading', 
-    bash_command='kneaddata -i /bioinformatics/dchen05/testing/HSM6XRUZ_R1.fastq.gz -i /bioinformatics/dchen05/testing/HSM6XRUZ_R2.fastq.gz -o /output/bioinformatics/dchen05/testing/output -db /isilon_biodata/dchen05/kneaddata', 
+    bash_command='kneaddata -i /home/ubuntu/project_data/microbiome/HSM6XRUZ_R1.fastq.gz -i /home/ubuntu/project_data/microbiome/HSM6XRUZ_R2.fastq.gz -o /home/ubuntu/project_data/microbiome/output -db /home/ubuntu/microbiome/kneaddata', 
     dag=dag)
