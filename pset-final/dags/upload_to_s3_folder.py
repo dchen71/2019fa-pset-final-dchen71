585df64eea373936bdeda51cbc961da53cc2349e
Ubuntu <ubuntu@ip-172-31-2-159.us-west-1.compute.internal>
2019-12-12 02:21:22 +0000

    version to upload a folders contents minus the folders

---
+++ pset-final/dags/upload_to_s3_folder.py
@@ -0,0 +1,34 @@
+'''
+Test Uploading to s3
+Testing the uploading portion of s3hook
+'''
+
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.hooks.S3_hook import S3Hook
+from airflow.models import DAG
+from datetime import datetime, timedelta
+import os
+
+# Basic arguments to pass to Airflow
+args = {
+    'owner': 'airflow',
+    'start_date': datetime.now() - timedelta(days = 1),
+}
+
+# Create the head dag
+dag = DAG(
+    dag_id='upload_folder', 
+    default_args=args,
+    schedule_interval=None)
+
+
+def upload(**kwargs):
+    s3 = S3Hook()
+    files = os.listdir('/home/ubuntu/output/')
+    [s3.load_file('/home/ubuntu/output/' + file_name. 'output/' + file_name, bucket_name = 'airflow-project', replace = True) for file_name in files if not os.path.isdir('/home/ubuntu/output/' + file_name)]
+
+task = PythonOperator(
+        python_callable = upload,
+        task_id = "upload",
+        dag = dag)
