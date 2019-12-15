dadf1a22ddf8755fc89d09e645bab34b317850c1
Ubuntu <ubuntu@ip-172-31-2-159.us-west-1.compute.internal>
2019-12-15 06:25:49 +0000

    fix text

--- pset-final/cli.py
+++ pset-final/cli.py
@@ -1,15 +1,16 @@
 '''
 cli.py
-Command line interface for travis to run tests
+Command line interface for running metagenomics pipeline
 '''
 
 import argparse
 import subprocess
 
-parser = argparse.ArgumentParser(description = "Enter name of sample to run paired end metagenomics pipeline")
+parser = argparse.ArgumentParser(description = "Enter name of samples to run paired end metagenomics pipeline")
 parser.add_argument("-s1", "--sample1")
 parser.add_argument("-s2", "--sample2")
 args = parser.parse_args()
 
 def main(args=args):
+    # Run subprocess to run bash process to call airflow and trigguer job
     subprocess.Popen(["airflow", "trigger_dag", "metagenomics_docker", "--conf", "{\"read_1\": \"" + args.sample1 + "\", \"read2_name\": \"" + args.sample2 + "\"}"])
