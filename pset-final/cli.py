d8b9199c30dbf55d71e115beee507c13eead32a0
Ubuntu <ubuntu@ip-172-31-2-159.us-west-1.compute.internal>
2019-12-15 06:40:12 +0000

    fix typo with 1st parameter

--- pset-final/cli.py
+++ pset-final/cli.py
@@ -13,4 +13,6 @@ args = parser.parse_args()
 
 def main(args=args):
     # Run subprocess to run bash process to call airflow and trigguer job
-    subprocess.Popen(["airflow", "trigger_dag", "metagenomics_docker", "--conf", "{\"read_1\": \"" + args.sample1 + "\", \"read2_name\": \"" + args.sample2 + "\"}"])
+    subprocess_args = ["airflow", "trigger_dag", "metagenomics_docker", "--conf", "{\"read1_name\": \"" + args.sample1 + "\", \"read2_name\": \"" + args.sample2 + "\"}"]
+    print(subprocess_args)
+    subprocess.Popen(subprocess_args, shell = False)
