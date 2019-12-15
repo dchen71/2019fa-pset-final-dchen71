34a80f5054f1fb1d1e82e4e79d394b99cb990682
Ubuntu <ubuntu@ip-172-31-2-159.us-west-1.compute.internal>
2019-12-15 06:24:17 +0000

    can submit jobs to airflow via cli

--- pset-final/cli.py
+++ pset-final/cli.py
@@ -4,10 +4,12 @@ Command line interface for travis to run tests
 '''
 
 import argparse
+import subprocess
 
 parser = argparse.ArgumentParser(description = "Enter name of sample to run paired end metagenomics pipeline")
-parser.add_argument("-s", "--sample")
+parser.add_argument("-s1", "--sample1")
+parser.add_argument("-s2", "--sample2")
 args = parser.parse_args()
 
 def main(args=args):
-    pass
+    subprocess.Popen(["airflow", "trigger_dag", "metagenomics_docker", "--conf", "{\"read_1\": \"" + args.sample1 + "\", \"read2_name\": \"" + args.sample2 + "\"}"])
